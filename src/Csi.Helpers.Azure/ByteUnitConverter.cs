adaf1adf4c390259070e12be800471afd0c93f7b
Dong Liu <doliu@microsoft.com>
2018-04-20 13:58:47 +0800

    Add ByteUnitConverter

---
+++ src/Csi.Helpers.Azure/ByteUnitConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Csi.Helpers.Azure
+{
+    /// <summary>
+    /// Azure disk and file service uses size in GiB, but csi api uses bytes, 
+    /// this is a helper class to convert between the two
+    /// </summary>
+    public class ByteUnitConverter
+    {
+        /// <summary>
+        /// Convert required byptes to GiB, do round up if size is less than 1 GiB.
+        /// </summary>
+        /// <param name="sizeInByte"></param>
+        /// <returns></returns>
+        public int ToGibibyte(long sizeInByte)
+        {
+            if (sizeInByte <= 0) sizeInByte = 1;
+            // round up
+            return (int)(((sizeInByte - 1) >> 30) + 1);
+        }
+
+        public long FromGigibyte(int sizeInGigibyte) => sizeInGigibyte << 30;
+    }
+}
---
+++ tests/Csi.Helpers.Azure.Tests/ByteUnitConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Csi.Helpers.Azure.Tests
+{
+    public class ByteUnitConverterTest
+    {
+        private static readonly ByteUnitConverter buc = new ByteUnitConverter();
+
+        [Theory]
+        [InlineData(1, 0L)]
+        [InlineData(1, 2L)]
+        [InlineData(1, 2L << 10)]
+        [InlineData(1, 1L << 30)]
+        [InlineData(2, (2L << 30) - 1)]
+        [InlineData(2, (2L << 30) + 0)]
+        [InlineData(3, (2L << 30) + 1)]
+        [InlineData(2 << 20, 2L << 50)]
+        public void ToGibibyte(int expected, long input) => Assert.Equal(expected, buc.ToGibibyte(input));
+
+        [Theory]
+        [InlineData(0L, 0)]
+        [InlineData(1L<<30, 1)]
+        public void FromGigibyte(long expected, int input) => Assert.Equal(expected, buc.FromGigibyte(input));
+    }
+}
