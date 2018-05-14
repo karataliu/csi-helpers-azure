549c066e95570ce0d29d5439082c564a7777cebd
Dong Liu <doliu@microsoft.com>
2018-05-14 15:22:10 +0800

    Update dependency

--- src/Csi.Helpers.Azure/AzureEnvironmentHelper.cs
+++ src/Csi.Helpers.Azure/AzureEnvironmentHelper.cs
@@ -1,27 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 
 namespace Csi.Helpers.Azure
 {
     public static class AzureEnvironmentHelper
     {
-        private static Dictionary<string, AzureEnvironment> envDic =
-            new Dictionary<string, AzureEnvironment>(StringComparer.OrdinalIgnoreCase)
-            {
-                [nameof(AzureEnvironment.AzureGlobalCloud)] = AzureEnvironment.AzureGlobalCloud,
-                [nameof(AzureEnvironment.AzureChinaCloud)] = AzureEnvironment.AzureChinaCloud,
-                [nameof(AzureEnvironment.AzureUSGovernment)] = AzureEnvironment.AzureUSGovernment,
-                [nameof(AzureEnvironment.AzureGermanCloud)] = AzureEnvironment.AzureGermanCloud,
-            };
-
         public static string DefaultEnvironmentName => nameof(AzureEnvironment.AzureGlobalCloud);
 
         public static AzureEnvironment ParseAzureEnvironment(string environmentName)
         {
             if (string.IsNullOrEmpty(environmentName)) return AzureEnvironment.AzureGlobalCloud;
-            if (envDic.TryGetValue(environmentName, out var env)) return env;
-            throw new Exception("Unknown environment: " + environmentName);
+
+            return AzureEnvironment.FromName(environmentName) 
+                ?? throw new Exception("Unknown environment: " + environmentName);
         }
 
         public static string GetStorageEndpointSuffix(string environmentName)
